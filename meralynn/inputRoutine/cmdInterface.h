3b3b3e74f4b3008483a27573eb9d37d8fbbece31
mcmohabir <mcmohabir@gmail.com>
2020-02-19 16:07:13 +0100

    non-blocking input routine finished

---
+++ meralynn/inputRoutine/cmdInterface.h
@@ -0,0 +1,22 @@
+#ifndef CMDINTERFACE_H
+#define CMDINTERFACE_H
+
+/*
+struct stub
+{
+    
+} commandStub;
+
+*/
+
+
+struct terminalCommands
+{
+    int cid;
+    String commandName;
+    //commandStub stub;
+} command;
+
+
+
+#endif
